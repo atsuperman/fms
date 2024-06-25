d987a57606a1d99510dad2d9fdacdc26ab1e34da
atsuperman <smstarvinoth@gmail.com>
2024-06-25 15:09:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const refreshButtons = document.querySelectorAll('.refresh-btn');
+    const musicSelector = document.getElementById('music-selector');
+    const iframeContainers = document.querySelectorAll('.grid-item');
 
     refreshButtons.forEach(button => {
         button.addEventListener('click', () => {
             const frameId = button.getAttribute('data-frame');
             const iframe = document.getElementById(frameId);
+            iframe.src = iframe.src; // Reload the iframe content
+        });
+    });
 
-            // Reload the iframe content
-            iframe.src = iframe.src;
+    musicSelector.addEventListener('change', (event) => {
+        const selectedFrameId = event.target.value;
+
+        iframeContainers.forEach(container => {
+            if (container.id === `container-${selectedFrameId}`) {
+                container.style.display = 'flex';
+            } else {
+                container.style.display = 'none';
+            }
         });
     });
 });
